7eeafe750d492fbcbd670ff50b4462764a887634
Yuliia Nivina <y.nivina@gmail.com>
2023-06-03 00:26:22 +0300

    Add HW-05 fix2

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <ContextMovie>
         <App />
       </ContextMovie>
