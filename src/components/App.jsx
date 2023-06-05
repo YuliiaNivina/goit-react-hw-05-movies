d82a36327eac3825b2d22b3ed45bf3d407f15982
Yuliia Nivina <y.nivina@gmail.com>
2023-06-05 12:45:09 +0300

    Add HW-05 fix5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 import { lazy } from 'react';
 import Layout from './Layout/Layout';
-import Home from '../pages/Home';
-import Movies from '../pages/Movies';
 
+const Home = lazy(() => import('../pages/Home'));
+const Movies = lazy(() => import('../pages/Movies'));
 const MovieDetail = lazy(() => import('../pages/MovieDetails'));
 const Cast = lazy(() => import('../pages/Cast'));
 const Reviews = lazy(() => import('../pages/Reviews'));
@@ -14,7 +14,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-
+          <Route path="*" element={<Home />} />
           <Route path="/movies" element={<Movies />} />
           <Route path="/movies/:movieId" element={<MovieDetail />}>
             <Route path="/movies/:movieId/cast" element={<Cast />} />
--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,4 +1,5 @@
 import { Link, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const MoviesList = ({ movies }) => {
   const location = useLocation();
@@ -17,3 +18,7 @@ const MoviesList = ({ movies }) => {
 };
 
 export default MoviesList;
+
+MoviesList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+};
